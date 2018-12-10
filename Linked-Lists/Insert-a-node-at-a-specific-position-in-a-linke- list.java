e2a87f2552498231acbecc15f21d7e95fd5d5e81
Amin Uddin <aminuddin01697@gmail.com>
2018-12-10 17:13:12 +0530

    Create Insert-a-node-at-a-specific-position-in-a-linke- list.java

---
+++ Linked-Lists/Insert-a-node-at-a-specific-position-in-a-linke- list.java
@@ -0,0 +1,23 @@
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+          SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+         SinglyLinkedListNode temp = head;
+          for(int i=0;i<position-1;i++){
+              temp = temp.next;
+          }
+         SinglyLinkedListNode temp1 = temp.next;
+         temp.next = node;
+         node.next = temp1;
+        return head;
+
+    }
